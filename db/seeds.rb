ad795456144c21dbfc8c84cf1f50dc0844bc3d2f
ash949 <hamzaashoor949@hotmail.com>
2017-11-17 02:40:01 +0200

    created and initialized server API with devise_auth_token and database configurations + seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create(email: 'test1@test1.com', nickname: 'test1', name: 'Test1 Test1', password: "12341234")
+user.save
